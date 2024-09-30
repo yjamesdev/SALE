8441aa13657039caf986778b263ddf54345f8469
Yirbelt <devyirbeltj@gmail.com>
2024-09-29 18:43:20 -0700

    First Commit

---
+++ backend/Controllers/Account.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using backend.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class Account : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public Account(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] Login model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Username);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+
+                var authClaims = new List<Claim>
+                {
+                  new Claim(JwtRegisteredClaimNames.Sub, user.UserName!),
+                  new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                };
+
+                authClaims.AddRange(userRoles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+                var token = new JwtSecurityToken(
+                    issuer: _configuration["Jwt:Issuer"],
+                    expires: DateTime.Now.AddMinutes(double.Parse(_configuration["JwT: ExpiryMinutes"]!)),
+                    claims: authClaims,
+                    signingCredentials: new SigningCredentials(
+                     new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!)),
+                      SecurityAlgorithms.HmacSha256
+                    )
+                );
+                return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+            }
+            return Unauthorized();
+        }
+
+        [HttpPost("add-role")]
+        public async Task<IActionResult> AddRole([FromBody] string role)
+        {
+            if (!await _roleManager.RoleExistsAsync(role))
+            {
+                var result = await _roleManager.CreateAsync(new IdentityRole(role));
+                if (result.Succeeded)
+                {
+                  return Ok(new {message = "Role Added Sucessfully"});
+                }
+                return BadRequest(result.Errors);
+            }
+            BadRequest("Role alredy Exists");
+        }
+
+        [HttpPost("assign-role")]
+        public async Task<IActionResult> AssignRole([FromBody] Role model) {
+            var user = await 
+        }
+    }
+}
